01ed8413abfcd8a62427c627b38ca2cdbf706818
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2026-02-08 23:20:21 -0600

    Agregando páginas nosotros y proceso

---
+++ src/types/index.ts
@@ -0,0 +1,43 @@
+import { z } from 'astro:content';
+
+const imageSchema = z.object({
+	url: z.string(),
+	width: z.number(),
+	height: z.number(),
+});
+
+const featureImagesSchema = z.object({
+	thumbnail: imageSchema,
+	medium: imageSchema,
+	medium_large: imageSchema,
+	large: imageSchema,
+	full: imageSchema,
+});
+
+export const BaseWPSchema = z.object({
+	id: z.number(),
+	title: z.object({
+		rendered: z.string(),
+	}),
+	content: z.object({
+		rendered: z.string(),
+	}),
+	featured_images: featureImagesSchema,
+	acf: z.object({
+		subtitle: z.string(),
+	}),
+});
+
+const processSchema = z.object({
+	title: z.string(),
+	description: z.string(),
+	image: z.string(),
+});
+
+export const ProcessPageSchema = BaseWPSchema.extend({
+	acf: z
+		.object({
+			subtitle: z.string(),
+		})
+		.catchall(processSchema),
+});
