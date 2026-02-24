f152ec9cc23fffc0d715642d66eb5d58e8bf7feb
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2026-02-24 00:17:18 -0600

    Preparando para netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
+
 	image: {
-		domains: ['coffeeshop.local'],
+		domains: ['coffeeshop.local', 'plum-tiger-200350.hostingersite.com'],
 	},
+
+	adapter: netlify(),
 });
